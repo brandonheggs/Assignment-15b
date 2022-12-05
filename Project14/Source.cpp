a0736f807919ef9445eda124d4594f3aa654354d
Kris Crona <brandon.heggs@yahoo.com>
2022-12-05 00:03:36 -0500

    Made the  main function and also fixed spelling mistake in the first if statement. Ran code and it worked.

--- Project14/Source.cpp
+++ Project14/Source.cpp
@@ -9,6 +9,31 @@ using namespace std;
 template <typename T>
 
 T half(T value)
+{  
+	T halved = value/2;
+
+	if (*typeid(value).name() == 'i')
+	{
+
+     if (halved * 2 != value)
+	{
+
+		halved++;
+    }
+    }
+	return halved;
+
+    }
+
+int main()
 {
+	double a = 7.0;
+	float b = 5.0f;
+	int c = 3;
+
+	cout << half(a) << endl;
+	cout << half(b) << endl;
+	cout << half(c) << endl;
+	return 0;
 
 }
